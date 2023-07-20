2c3e47b75d494ce7937a86b12d54d3f9839c5e3e
elecun <elecun2@gmail.com>
2023-07-20 17:45:22 +0900

    updated

---
+++ include/flame/log.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file log.hpp
+ * @author Byunghun Hwang (bh.hwang@iae.re.kr)
+ * @brief Print log data on console
+ * @version 0.1
+ * @date 2023-07-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _FLAME_INCLUDE_LOG_HPP_
+#define _FLAME_INCLUDE_LOG_HPP_
+
+#endif
