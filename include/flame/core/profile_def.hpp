15dbeb4c5cc5c385b7d93b22243647209783e7db
elecun <elecun2@gmail.com>
2023-07-24 08:52:13 +0900

    add definitions for profile

---
+++ include/flame/core/profile_def.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file profile_def.hpp
+ * @author Byunghun Hwang(bh.hwang@iae.re.kr)
+ * @brief Definitions for profile
+ * @version 0.1
+ * @date 2023-07-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _FLAME_CORE_PROFIEL_DEF_HPP_
+#define _FLAME_CORE_PROFIEL_DEF_HPP_
+
+
+/* Configuration file keys for components */
+#define _CONFIG_ENV_KEY_ "environments"
+#define _CONFIG_PATH_KEY_ "path"
+#define _CONFIG_REQ_KEY_  "required"
+#define _CONFIG_COMPONENTS_KEY_ "components"
+#define _PROFILE_CONFIGURATIONS_KEY_ "configurations"
+
+#endif
