818f9562cc7e9439128d528a83a34e587335e125
elecun <elecun2@gmail.com>
2025-04-18 12:02:22 +0900

    add

---
+++ include/device/bus.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file bus.hpp
+ * @author byunghun hwang <bh.hwang@iae.re.kr>
+ * @brief device bus common interface
+ * @version 0.1
+ * @date 2025-04-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+
+ #ifndef FLAME_DEVICE_BUS_HPP_INCLUDED
+ #define FLAME_DEVICE_BUS_HPP_INCLUDED
+
+ namespace flame::device::bus {
+    class bus {
+    };
+ }
+
+ #endif
